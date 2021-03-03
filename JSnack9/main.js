2546d9b5d6dde24f3cd4e547550462429ebbb016
Lorenzo Bernini <templarec@gmail.com>
2021-03-03 16:37:39 +0100

    JSnack9

---
+++ JSnack9/main.js
@@ -0,0 +1,6 @@
+var somma = 0;
+for (var i = 1; i <= 10; i++) {
+  somma = somma + i;
+}
+console.log("La somma dei primi 10 numeri è: " + somma);
+console.log("La media dei primi 10 numeri è: " + somma / 10);
